3104d6e6072aa40ae991507246239870755eabf4
AbdelRahman <AbdelRahman@AbdelRahman-Lap.wotech-eg.loc>
2025-05-05 14:48:50 +0300

    Add Static Files

--- src/Project/settings.py
+++ src/Project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#Static Folder For All Project
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+    'D:\Private\FreeLance\python\Django\Blogger\src\static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
