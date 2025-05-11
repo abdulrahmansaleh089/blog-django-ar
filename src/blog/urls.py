778286a3d31f006a7b99e9e1dcc48639508dfd58
AbdelRahman <AbdelRahman@AbdelRahman-Lap.wotech-eg.loc>
2025-05-11 22:45:13 +0300

    create post_model

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about_us, name='about_us'),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,43 @@
 from django.shortcuts import render
-
+posts = [
+    {
+        'title': 'التدوينة الأولي',
+        'content': 'هذه هي محتوي التدوينة الأولي',
+        'date_posted': '2023-10-01',
+        'author': 'أحمد',
+    },
+        {
+        'title': 'التدوينة الثانية',
+        'content': 'هذه هي محتوي التدوينة الثانية',
+        'date_posted': '2018-09-06',
+        'author': 'كريم',
+    },
+        {
+        'title': 'التدوينة الثالثة',
+        'content': 'هذه هي محتوي التدوينة الثاثلة',
+        'date_posted': '2024-03-28',
+        'author': 'علي',
+    },
+        {
+        'title': 'التدوينة الرابعة',
+        'content': 'هذه هي محتوي التدوينة الرابعة',
+        'date_posted': '2021-12-15',
+        'author': 'تامر',
+    },
+        {
+        'title': 'التدوينة الخامسة',
+        'content': 'هذه هي محتوي التدوينة الخامسة',
+        'date_posted': '2022-09-01',
+        'author': 'سيد'}
+]
 def home(request):
-    return render(request,'blog/index.html',{'title':'home'})
+    context = {
+        'posts': posts,
+        'title': 'الصفحة الرئيسية',
+    }
+    return render(request, 'blog/index.html', context)
+
+
+def about_us(request):
+
+    return render(request, 'blog/about.html', {'title': 'من نحن'})
